529ae69c5f110500603ce08a3ace62f3262f239d
Ayato <superstickhuman0@gmail.com>
2017-05-10 17:43:07 +0900

    authorization

--- HelloTwitter.py
+++
@@ -1,8 +0,0 @@
-#-*- coding: utf-8 -*-
-import tweepy
-
-auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
-auth.set_access_token(access_token, access_secret)
-api = tweepy.API(auth)
-
-api.update_status("Hello, twitter!")
--- twitter.py
+++
@@ -1,9 +0,0 @@
-#-*- coding: utf-8 -*-
-import tweepy
-
-auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
-print("Access:", auth.get_authorization_url())
-verifier = input("Verifier:")
-auth.get_access_token(verifier)
-print("Access Token:", auth.access_token)
-print("Access Token Secret:", auth.access_token_secret)
